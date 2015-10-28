30caacc66b94d54437e7ece135502e60adc02c87
songjundev <songjun0810@163.com>
2015-10-28 12:29:33 +0800

    loginserver

--- code/server/LoginServer/source/LoginServer.cpp
+++ code/server/LoginServer/source/LoginServer.cpp
@@ -11,6 +11,8 @@ createFileSingleton(CLog);
 createFileSingleton(CLuaEngine);
 createFileSingleton(CLoginServer);
 
+CObjectMemoryPool<PACKET_COMMAND>	g_PacketPool;
+
 static int httpserver_ev_handler(struct mg_connection *conn, enum mg_event ev) {	if (ev == MG_REQUEST) {		Log.Debug("[HttpServer]Handle Event:%d ip:%s", ev, conn->remote_ip);		if (true) {			mg_send_data(conn, "0", 1);		} else {			mg_send_data(conn, "1", 1);
 		}		return MG_TRUE;	}	else if (ev == MG_AUTH) {		return MG_TRUE;	}	else {		return MG_FALSE;	}}
 
